5b5fb29636b174203808d65fcc1e0ed7f5b22389
Pablo <126351137+pabloqr@users.noreply.github.com>
2025-06-15 12:19:02 +0200

    Added guards and decorators in auth module

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -17,18 +17,20 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   }
 
   async validate(payload: { sub: number; mail: string }) {
+    // Se trata de obtener el usuario dado su identificador
     const user = await this.prisma.users.findUnique({
       where: {
         id: payload.sub,
       },
     });
 
+    // Si no existe, se devuelve un objeto vacío
     if (!user) {
       return {};
     }
 
+    // Se elimina la contraseña del objeto y se devuelve
     const { password: _, ...userWithoutPassword } = user;
-
     return userWithoutPassword;
   }
 }
